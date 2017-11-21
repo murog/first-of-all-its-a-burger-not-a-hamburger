bc3c1632cab9d918a16935be2070da85aaa1dc6e
murog <guillermina.muro@gmail.com>
2017-11-21 11:51:06 -0800

    working dynamic z-index on hamburger

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,31 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function() {
+  $(function() {
+    $(".draggable").draggable();
+  });
 
-$(function() {
-  $(".draggable").draggable();
+  $(`.draggable`).on('mouseenter', function() {
+    console.log('stuff is happening');
+    $(this).css({'z-index': "999"});
+  });
 });
+
+
+
+
+
+
+// $(function zIndex() {
+//     let draggables = $(".draggable");
+//     draggables.mouseenter(function() {
+//         var el = $(this);
+//             max = 0;
+//         draggables.each(function() {
+//             var z = parseInt( $( this ).css("z-index"), 10);
+//             max = Math.max(max, z);
+//         });
+//         el.css("z-index", max + 1);
+//     });
+//   })
