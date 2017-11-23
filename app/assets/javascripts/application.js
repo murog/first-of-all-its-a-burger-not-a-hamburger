d37a1bc1e36ba559849a293fca37dcf10748d4e5
murog <guillermina.muro@gmail.com>
2017-11-22 17:52:22 -0800

    transpile javascript into es5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,64 +16,59 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//
-$(document).ready(function() {
-  $(function() {
+
+$(document).ready(function () {
+  $(function () {
     $(".draggable").draggable();
   });
-_}); //this is fake end
-//   const setZIndexByArrayIndex = function setZIndexByArrayIndex(array) {
-//     for(let i = 0; i < array.length; i++) {
-//       $(array[i]).css({'z-index': `${i}`});
-//     }
-//   };
-//   // creates a real array for draggables
-//   const dragArray = [];
-//   for(let i = 0; i < $('.draggable').length ; i++){
-//       dragElement = $(".draggable")[i];
-//       dragArray.push(dragElement);
-//       setZIndexByArrayIndex(dragArray);
-//   };
-//
-//   collectPositions();
-//
-//
-//   $(`.draggable`).on('click', function() {
-//     // look for it in dragArray
-//     const current = this;
-//     for(let i = 0; i< dragArray.length; i++ ){
-//       if (dragArray[i] === current) {
-//         const splicedIt = dragArray.splice(i, 1);
-//         dragArray.push(splicedIt[0]);
-//         setZIndexByArrayIndex(dragArray);
-//       }
-//     };
-//     collectPositions();
-//
-//   });
-// });
-//
-//
-//
-// const collectPositions = function collectPositions(){
-//   const mood = $('#mood-container');
-//   mood.top = 100;
-//   mood.bottom = 500;
-//   mood.left = 0;
-//   mood.right = 400;
-//   const draggables = $('.draggable');
-//   const moodItems = [];
-//   for(i=0; i< draggables.length; i++) {
-//     const item = draggables[i];
-//     item.left = item.style.left;
-//     const leftEnd = item.left.length -2;
-//     item.left = parseFloat(item.left.substring(0, leftEnd));
-//     item.top = item.style.top;
-//     const topEnd = item.top.length -2;
-//     item.top = parseFloat(item.top.substring(0, topEnd));
-//     if((item.top <= mood.bottom) && (item.top >= mood.top) && (item.left <= mood.right)) {
-//       moodItems.push(draggables[i]);
-//     }
-//   }
-//   console.log(`there are ${moodItems.length} items in the container`);
-// }
+  var setZIndexByArrayIndex = function setZIndexByArrayIndex(array) {
+    for (var _i = 0; _i < array.length; _i++) {
+      $(array[_i]).css({ 'z-index': '' + _i });
+    }
+  };
+  // creates a real array for draggables
+  var dragArray = [];
+  for (var _i2 = 0; _i2 < $('.draggable').length; _i2++) {
+    dragElement = $(".draggable")[_i2];
+    dragArray.push(dragElement);
+    setZIndexByArrayIndex(dragArray);
+  };
+
+  collectPositions();
+
+  $('.draggable').on('click', function () {
+    // look for it in dragArray
+    var current = this;
+    for (var _i3 = 0; _i3 < dragArray.length; _i3++) {
+      if (dragArray[_i3] === current) {
+        var splicedIt = dragArray.splice(_i3, 1);
+        dragArray.push(splicedIt[0]);
+        setZIndexByArrayIndex(dragArray);
+      }
+    };
+    collectPositions();
+  });
+});
+
+var collectPositions = function collectPositions() {
+  var mood = $('#mood-container');
+  mood.top = 100;
+  mood.bottom = 500;
+  mood.left = 0;
+  mood.right = 400;
+  var draggables = $('.draggable');
+  var moodItems = [];
+  for (i = 0; i < draggables.length; i++) {
+    var item = draggables[i];
+    item.left = item.style.left;
+    var leftEnd = item.left.length - 2;
+    item.left = parseFloat(item.left.substring(0, leftEnd));
+    item.top = item.style.top;
+    var topEnd = item.top.length - 2;
+    item.top = parseFloat(item.top.substring(0, topEnd));
+    if (item.top <= mood.bottom && item.top >= mood.top && item.left <= mood.right) {
+      moodItems.push(draggables[i]);
+    }
+  }
+  console.log('there are ' + moodItems.length + ' items in the container');
+};
