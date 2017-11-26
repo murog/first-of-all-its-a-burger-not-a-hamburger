87fb6ad7f692090f4388236ee672a4c802f16c09
murog <guillermina.muro@gmail.com>
2017-11-25 17:52:53 -0800

    created moods controller

---
+++ app/controllers/moods_controller.rb
@@ -0,0 +1,6 @@
+class MoodsController < ApplicationController
+  # def new
+  # end
+  def create
+  end
+end
---
+++ test/controllers/moods_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe MoodsController do
+  # it "must be a real test" do
+  #   flunk "Need real tests"
+  # end
+end
