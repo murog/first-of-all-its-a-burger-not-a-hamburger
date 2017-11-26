49d00fd6cd40a9dfc9589ca1266c00c2d0d3461a
murog <guillermina.muro@gmail.com>
2017-11-25 16:25:46 -0800

    passing validation tests for mood_items

--- app/models/mood_item.rb
+++ app/models/mood_item.rb
@@ -1,2 +1,6 @@
 class MoodItem < ApplicationRecord
+  validates :top_coord, presence: true
+  validates :left_coord, presence: true
+  validates :z_index, presence: true
+  
 end
--- test/models/mood_item_test.rb
+++ test/models/mood_item_test.rb
@@ -3,7 +3,17 @@ require "test_helper"
 describe MoodItem do
   let(:mood_item) { MoodItem.new }
 
-  it "must be valid" do
-    value(mood_item).must_be :valid?
+  it "must be invalid without everything" do
+    mood_item.valid?.must_equal false
+    mood_item.mood_id = moods(:first_valid_mood).id
+    mood_item.valid?.must_equal false
+    mood_item.item_id = items(:first_burger)
+    mood_item.valid?.must_equal false
+    mood_item.top_coord = 40
+    mood_item.valid?.must_equal false
+    mood_item.left_coord = 40
+    mood_item.valid?.must_equal false
+    mood_item.z_index = 1
+    mood_item.valid?.must_equal true
   end
 end
