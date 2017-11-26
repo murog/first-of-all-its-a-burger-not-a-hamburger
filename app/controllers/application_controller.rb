324f4ae1c5b83592684ff0c8a4905c303b49b6f8
murog <guillermina.muro@gmail.com>
2017-11-25 17:53:51 -0800

    created generate items method in application controller to generate random items for mood

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def index
-    # random_images =[]
+    @mood = Mood.new
+    generate_items(4)
+
+
+  end
+  private
+  def generate_items(num)
+    all_items = Item.all
+    all_items.shuffle
+    @items = []
+    num.times do |i|
+      return if all_items[i].nil?
+      @items << all_items[i]
+    end
   end
 end
