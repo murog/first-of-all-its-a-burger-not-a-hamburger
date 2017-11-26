c03fd3c2f3dab924331fac48df6383b359b4f839
murog <guillermina.muro@gmail.com>
2017-11-25 16:04:04 -0800

    passing tests for mood validations, created mood ymls

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,2 +1,6 @@
 class Mood < ApplicationRecord
+  validates :name, presence: true
+  validates :description, length: 0..140, allow_blank: true
+
+
 end
--- test/models/mood_test.rb
+++ test/models/mood_test.rb
@@ -3,7 +3,37 @@ require "test_helper"
 describe Mood do
   let(:mood) { Mood.new }
 
-  it "must be valid" do
-    value(mood).must_be :valid?
+  it "must be invalid without name" do
+    mood.valid?.must_equal false
+    mood.name = "the new mood"
+    mood.valid?.must_equal true
+  end
+
+  it "must be invalid if description is greater than 140 characters" do
+    moods(:mood_too_long).valid?.must_equal false
+  end
+
+  it "will be valid if description is blank" do
+    mood.valid?.must_equal false
+    mood.name = "the new mood"
+    mood.valid?.must_equal true
+    mood.description = ""
+    mood.valid?.must_equal true
+  end
+
+  it "will be valid if description under 140 characters" do
+    mood.valid?.must_equal false
+    mood.name = "the new mood"
+    mood.valid?.must_equal true
+    mood.description = "140 characters is good"
+    mood.valid?.must_equal true
+  end
+
+  it "will be valid with 140 character description" do
+    mood.valid?.must_equal false
+    mood.name = "the new mood"
+    mood.valid?.must_equal true
+    mood.description = "one hundred and forty one hundred and forty one hundred and forty one hundred and forty one hundred and forty one hundred and forty one one!"
+    mood.valid?.must_equal true
   end
 end
