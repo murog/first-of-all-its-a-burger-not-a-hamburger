1a9afa949107eb730f93d3c72d23ddd708e70103
murog <guillermina.muro@gmail.com>
2017-11-25 16:17:12 -0800

    passing tests for item validations, created item ymls

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
+  validates :alt_text, presence: true
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -3,7 +3,25 @@ require "test_helper"
 describe Item do
   let(:item) { Item.new }
 
-  it "must be valid" do
-    value(item).must_be :valid?
+  it "must be invalid without name, url and alt_text" do
+    item.valid?.must_equal false
+    item.name = "second_burger"
+    item.valid?.must_equal false
+    item.url = "second_burger.gif"
+    item.valid?.must_equal false
+    item.alt_text = "it's a second_burger"
+    item.valid?.must_equal true
   end
+
+  it "must be invalid if name and url are not unique" do
+    item.valid?.must_equal false
+    item.name = "first_burger"
+    item.url = "first_burger.gif"
+    item.alt_text = "it's a first_burger"
+    item.valid?.must_equal false
+    item.name = "unique_burger"
+    item.url = "unique_burger.gif"
+    item.valid?.must_equal true
+  end
+
 end
