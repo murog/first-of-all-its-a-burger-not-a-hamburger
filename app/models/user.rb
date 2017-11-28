8d9bee516250cc20809ec461298bc4004a911a9b
Tanisha <tanishahampden@gmail.com>
2017-11-27 19:31:19 -0800

    add method to retreive auth hash info

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :uid, presence: true
   validates :provider, presence: true
+
+  def self.from_auth_hash(provider, auth_hash)
+    user = new
+    user.provider = provider
+    user.uid = auth_hash['uid']
+    user.username = auth_hash['info']['name'] || auth_hash['info']['nickname']
+    user.email = auth_hash['info']['email']
+
+    return user
+  end
+  
 end
