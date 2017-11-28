19c2f2e9bfd9409466fd4b502e09ca711020af7c
Tanisha <tanishahampden@gmail.com>
2017-11-27 18:44:55 -0800

    add sessions#create route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :moods
   root 'application#index'
+  get "/auth/:provider/callback", to: "sessions#create"
 
   # match '/moods' => 'moods#create', :via => :post
 
