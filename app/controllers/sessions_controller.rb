5457d11d954ed1a7b2b3f8b6c6bd325a58edb6e9
Tanisha <tanishahampden@gmail.com>
2017-11-27 19:32:00 -0800

    update sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,29 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+      if user.nil?
+        user = User.from_auth_hash(params[:provider], auth_hash)
+        user.save
+      else
+        flash[:status] = :success
+        flash[:result_text] = "Successfully logged in as existing user #{user}"
+        redirect_to root_path
+      end
+
+      session[:user_id] = user.id
+    else
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Could not log in"
+      flash.now[:messages] = user.errors.messages
+      redirect_to root_path
+    end
+  end
+
+  def index
+    @user = User.find(session[:user_id]) # < recalls the value set in a previous request
+  end
 end
