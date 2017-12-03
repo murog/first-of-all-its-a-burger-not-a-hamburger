1ce968859a186f6b487c6cf8ae41462a82e0f8ab
Tanisha <tanishahampden@gmail.com>
2017-12-02 22:49:53 -0800

    add prompt relation to mood

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,9 +1,9 @@
 class Mood < ApplicationRecord
   has_many :mood_items
   has_many :items, through: :mood_items
-  
   validates :name, presence: true
   validates :description, length: 0..140, allow_blank: true
   belongs_to :user, optional: true
+  belongs_to :prompt, optional: true
 
 end
