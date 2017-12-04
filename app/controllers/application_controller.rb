91042bcfd287fb98402cc88ff019884a7152c2f5
murog <guillermina.muro@gmail.com>
2017-12-03 23:25:46 -0800

    added find user method before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :find_user
+
 
   def index
     @mood = Mood.new
@@ -27,4 +29,12 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: "/public/404.html", status: 404
   end
+
+  def find_user
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
+  end
+
+
 end
