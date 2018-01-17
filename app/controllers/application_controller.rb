ef56f7eb66f5857a90627b255241d6d9e90f8e02
murog <guillermina.muro@gmail.com>
2018-01-17 11:47:16 -0800

    it's a birthday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,13 @@ class ApplicationController < ActionController::Base
 
   def index
     @mood = Mood.new
-    generate_items(5)
-    generate_prompt
-
+    # generate_items(5)
+    # generate_prompt
+    generate_bday_prompt
+    generate_bday_items(4)
 
   end
+
   private
   def generate_prompt
     prompts = Prompt.all
@@ -25,6 +27,16 @@ class ApplicationController < ActionController::Base
       @items << all_items[i]
     end
   end
+  def generate_bday_items(num)
+    all_items = Item.all.shuffle
+    # all_items.shuffle!
+    @items = []
+    num.times do |i|
+      return if all_items[i].nil?
+      @items << all_items[i]
+    end
+    @items << Item.find(73)
+  end
 
   def render_404
     render file: "/public/404.html", status: 404
@@ -34,6 +46,19 @@ class ApplicationController < ActionController::Base
     if session[:user_id]
       @user = User.find_by(id: session[:user_id])
     end
+  end
+
+  def generate_bday_prompt
+    bday_prompts = Prompt.where("id > ?", 32)
+    @prompt = bday_prompts.shuffle[0]
+    if (!@prompt)
+      temp_bday = Prompt.new
+      temp_bday.text = "happy bday isaac, stay cool"
+      temp_bday.save
+      @prompt = temp_bday
+    end
+
+
   end
 
 
